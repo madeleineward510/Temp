da9a01038a95561752c4e3029f80fa0df46fca07
madeleineward510 <44490471+madeleineward510@users.noreply.github.com>
2018-10-26 15:37:26 +0900

    Batch update

--- js/script.js
+++ js/script.js
@@ -93,5 +93,7 @@
         */       }
             });
         }); 
-    
+    <!--BEGIN POPPYDOG.COM PASSWORD PROTECT -->
+<script language="JavaScript" src="http://www.poppydog.com/ma/pd/pdutil.js">
+pd_isPP(46607,89761);
 pd_isPP(46607,89761);
